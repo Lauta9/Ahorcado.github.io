3283202f6d6735f938844e3de1f998f5562c3ae7
Lautaro <110798247+Lauta9@users.noreply.github.com>
2022-09-07 09:22:40 -0300

    JuegoCompleto

---
+++ JavaScript/palabras.js
@@ -0,0 +1,11 @@
+var listaPalabras = ["CASA","SILLON","JAVA","MESA","BARCO","CABEZA","LENGUA",
+                    "CODIGO","MARCO","CAMPERA","SALADO","CARRO","MALA",
+                    "PESA","CARNE","ALURA","LATAM"];
+
+function agregarPalabranueva(palabra){
+    listaPalabras.push(palabra);
+}
+
+function vaciarLista(){
+    listaPalabras = [];
+}
